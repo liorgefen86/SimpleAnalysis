947827018a93fa25065565683dd5dffc410dce8a
rookieds <lior@gefen.tech>
2020-05-24 02:12:13 +0200

    moved logic of creating main widget to its respective method updated method to load data and it now display information about the data

--- user_interface.py
+++ user_interface.py
@@ -1,10 +1,11 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, \
-    QFileDialog, QWidget, QStatusBar, QHBoxLayout
+    QFileDialog, QWidget, QStatusBar, QHBoxLayout, QGridLayout, QLabel
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 import sys
 import platform
 import os
+from data_analysis import DataAnalysis
 
 
 class UserInterface(QMainWindow):
@@ -47,29 +48,43 @@ class UserInterface(QMainWindow):
         statusbar.showMessage(self.os_text)
         self.setStatusBar(statusbar)
 
+        self.__set_main_widget()
+
+        # call method to display application
+        self.__show_application()
+
+    def __set_main_widget(self):
         # creation of a widget to hold the buttons
-        frame = QWidget(self)
-        frame.setGeometry(0, 0, size[0], size[1]-50)
+        main_widget = QWidget(self)
 
         # select button will open a filedialog to let the user select the file
         self.btn_select = QPushButton('Select file')
-        self.btn_select.setIcon(QIcon('icons/load.png'))
+        self.btn_select.setIcon(QIcon('icons/select.png'))
         self.btn_select.pressed.connect(self.__select_file)
+        self.btn_select.setMaximumWidth(150)
 
         # load button will load the selected file and start its processing
         self.btn_load = QPushButton('Load file')
         self.btn_load.setIcon(QIcon('icons/load.png'))
         self.btn_load.pressed.connect(self.__load_file)
+        self.btn_load.setMaximumWidth(150)
 
-        # creating an horizonal box to display the button on
+        # creating the main layout
+        self.main_layout = QGridLayout()
+
+        # creating an horizonal box to display the buttons on
         hbox = QHBoxLayout()
         hbox.addWidget(self.btn_select)
         hbox.addWidget(self.btn_load)
         hbox.setAlignment(Qt.AlignTop)
-        frame.setLayout(hbox)
+        hbox.setContentsMargins(0, 0, 0, 0)
 
-        # call method to display application
-        self.__show_application()
+        self.main_layout.addLayout(hbox, 0, 0)
+        self.main_layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        self.main_layout.setContentsMargins(10, 0, 10, 0)
+
+        main_widget.setLayout(self.main_layout)
+        self.setCentralWidget(main_widget)
 
     def __select_file(self):
         """
@@ -77,6 +92,12 @@ class UserInterface(QMainWindow):
         Return:
             None
         """
+        if hasattr(self, 'da'):
+            for ind in range(self.main_layout.count()):
+                layout_item = self.main_layout.itemAt(ind)
+                if layout_item.objectName() == 'grid_stats':
+                    self.main_layout.removeItem(layout_item)
+
         file_dialog = QFileDialog.Options()
         file_dialog |= QFileDialog.DontUseNativeDialog
         self.fileName, _ = QFileDialog.getOpenFileName(
@@ -98,13 +119,33 @@ class UserInterface(QMainWindow):
             None
         """
         if self.fileName:
-            pass
+            self.da = DataAnalysis.create_object(file_path=self.fileName)
+            self.da.get_statistics()
+
+            stats = self.da.stats
+
+            grid = QGridLayout()
+            grid.setObjectName('grid_stats')
+            fields = stats.columns
+            qties = stats.index
+
+            for indf, field in enumerate(fields, 1):
+                grid.addWidget(QLabel(field), indf, 0)
+            for indq, qty in enumerate(qties, 1):
+                grid.addWidget(QLabel(qty), 0, indq)
+
+            for indf, field in enumerate(fields):
+                for indq, qty in enumerate(qties):
+                    grid.addWidget(QLabel(str(stats.iloc[indq, indf])), 1+indf, 1+indq)
+
+            self.main_layout.addLayout(grid, 1, 0, 1, 2)
+
         else:
             self.statusBar().showMessage(self.os_text + '\t\t\tNo file selected')
 
     def __show_application(self):
         """
-        method used for displaying the application
+        method used for displaying the application<<<<<
         Return:
              None
         """
